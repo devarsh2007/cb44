3f88cec7db9e57d38b669b7e0f3583a4b6fc57b9
devarsh2007 <sunilpatel2131@gmail.com>
2024-12-23 19:55:44 +0530

    function

---
+++ function1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// without argument without return value function
+// define function
+void PrintLine()
+{
+    printf("\n---------------------------\n");
+}
+
+void main()
+{
+    // calling a function
+    PrintLine();
+    printf("hello world");
+    PrintLine();
+
+    printf("welcome user");
+    PrintLine();
+    PrintLine();
+}
